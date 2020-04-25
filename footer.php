cb4f0bfb76c58d36ee1b1c39248561cf30f4d32c
kurocraft0312 <kurocraft0312@gmail.com>
2020-04-25 21:17:35 +0900

    フッターをパーツ化。

---
+++ footer.php
@@ -0,0 +1,7 @@
+<?php wp_footer(); ?>
+<footer>
+    <div class="copyright">
+        <small lang="en">&copy; <?php echo date("Y"); ?> Ratpost by Kurokawa Shun</small>
+        <p>WordPress Ratpost_night Theme is provided by "<a class="official-link" href="https://www.rat-post-official.com/" target="_blank">Ratpost</a>".</p>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -189,13 +189,7 @@
             </section>
         </div>
     </main>
-    <footer>
-        <div class="copyright">
-            <small lang="en">&copy; <?php echo date("Y"); ?> Ratpost by Kurokawa Shun</small>
-            <p>WordPress Ratpost_night Theme is provided by "<a class="official-link" href="https://www.rat-post-official.com/" target="_blank">Ratpost</a>".</p>
-        </div>
-    </footer>
-    <?php wp_footer(); ?>
+    <?php get_footer(); ?>
     <script src="assets/js/jquery-3.4.1.min.js"></script>
     <script defer src="assets/js/index.js"></script>
 </body>
