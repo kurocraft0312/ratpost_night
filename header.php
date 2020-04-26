24a461043339ed723c58eb80617ad832f451e978
kurocraft0312 <kurocraft0312@gmail.com>
2020-04-26 11:06:08 +0900

    OGP・retinaディスプレイ対応版に変更。

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- OGP利用宣言 -->
-    <head prefix="og: https://ogp.me/ns# fb: https://ogp.me/ns/fb# article: https://ogp.me/ns/article#">
+    <head prefix="og: https://ogp.me/ns# fb: https://ogp.me/ns/fb# website: https://ogp.me/ns/website# article: https://ogp.me/ns/article#">
     <!-- BASIC -->
     <meta name="description" content="Night WordPress theme.">
     <meta name="keywords" content="Night WordPress theme,WordPress theme">
@@ -19,25 +19,21 @@
     <meta name="csrf-param" content="">
     <meta name="csrf-token" content="">
     <!-- OGP -->
-    <meta property="og:type" content="website">
-    <meta property="og:site_name" content="Ratpost_night">
-    <meta property="og:title" content="Night WordPress theme.">
-    <meta property="og:description" content="Night WordPress theme.">
-    <meta property="og:url" content="ページURL">
-    <meta property="og:image" content="画像URL">
-    <meta property="og:image:alt" content="画像の説明URL">
-    <meta property="og:local" content="ja_JP">
-    <!-- SNS -->
-    <meta property="fb:app_id" content="APP-ID（15文字）">
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:site" content="@kurocraft0312">
-    <meta name="twitter:image" content="OGP画像のURL">
-    <!-- canonical -->
+    <?php require_once('lib/admin/ogp.php'); ?>
+    <!-- canonical（内容が同じページがある時にURLを統一する。主にSSL化の際に用いられる） -->
     <link rel="canonical" href="">
     <!-- favicon -->
     <link rel="icon" href="./favicon.ico">
     <!-- Web Clip(Retina対応) -->
-    <link rel="apple-touch-icon" href="">
+    <link rel="apple-touch-icon" href="Web Clip用のアイコンURL貼る">
+    <link rel="apple-touch-icon" sizes="57x57" href="iPhone 3G">
+    <link rel="apple-touch-icon" sizes="72x72" href="非Retina版のiPad 2、iPad miniの用、iOS 6以前">
+    <link rel="apple-touch-icon" sizes="76x76" href="非Retina版のiPad 2、iPad miniの用、iOS 7以降">
+    <link rel="apple-touch-icon" sizes="114x114" href="Retina版のiPhone、iPod touch用、iOS 6以前">
+    <link rel="apple-touch-icon" sizes="120x120" href="Retina版のiPhone、iPod touch用、iOS 7以降">
+    <link rel="apple-touch-icon" sizes="144x144" href="Retina版のiPad、iPad mini用、iOS 6以前">
+    <link rel="apple-touch-icon" sizes="152x152" href="Retina版のiPad、iPad mini用、iOS 7以降">
+    <link rel="apple-touch-icon" sizes="180x180" href="iPhone 6 plus">
     <!-- CSS -->
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>/../assets/css/reset.min.css">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
--- lib/admin/ogp.php
+++ lib/admin/ogp.php
@@ -1,3 +1,50 @@
 <?php
+function custom_meta_ogp() {
+    if ( is_front_page() || is_home() || is_singular() ) {
+        global $post;
+        $ogp_title = '';
+        $ogp_descr = '';
+        $ogp_url = '';
+        $ogp_img = '';
+        $insert = '';
+  
+    if ( is_singular() ) { //記事＆固定ページ
+        setup_postdata($post);
+        $ogp_title = $post->post_title;
+        $ogp_descr = mb_substr(get_the_excerpt(), 0, 120);
+        $ogp_url = get_permalink();
+        wp_reset_postdata();
+    } elseif ( is_front_page() || is_home() ) { //トップページ
+        $ogp_title = get_bloginfo('name');
+        $ogp_descr = get_bloginfo('description');
+        $ogp_url = home_url();
+    }
 
+    //og:type
+    $ogp_type = ( is_front_page() || is_home() ) ? 'website' : 'article';
+
+    //og:image
+    if ( is_singular() && has_post_thumbnail() ) {
+        $ps_thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full');
+        $ogp_img = $ps_thumb[0];
+    } else {
+        //例）get_template_directory_uri().'/img/logo2.png';
+        $ogp_img = 'TOPページ＆アイキャッチ画像がないときに使われる画像のURL';
+        }
+
+        $insert .= '<meta property="og:title" content="'.esc_attr($ogp_title).'">' . "\n";
+        $insert .= '<meta property="og:description" content="'.esc_attr($ogp_descr).'">' . "\n";
+        $insert .= '<meta property="og:type" content="'.$ogp_type.'">' . "\n";
+        $insert .= '<meta property="og:url" content="'.esc_url($ogp_url).'">' . "\n";
+        $insert .= '<meta property="og:image" content="'.esc_url($ogp_img).'">' . "\n";
+        $insert .= '<meta property="og:site_name" content="'.esc_attr(get_bloginfo('name')).'">' . "\n";
+        $insert .= '<meta name="twitter:card" content="summary_large_image">' . "\n";
+        $insert .= '<meta name="twitter:site" content="ツイッターのアカウント名">' . "\n";
+        $insert .= '<meta property="og:locale" content="ja_JP">' . "\n";
+        $insert .= '<meta property="fb:app_id" content="ここにappIDを入力">' . "\n";
+
+        echo $insert;
+    }
+  } 
+add_action('wp_head','custom_meta_ogp');
 ?>
