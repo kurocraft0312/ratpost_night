a95bedb70da831e9bcaf054825bafba08f72c3cd
kurocraft0312 <kurocraft0312@gmail.com>
2020-04-29 22:16:05 +0900

    Gulp処理記載中。

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
-/*開発の最後に調整する→しばらくはEasy SassでOK*/
-function defaultTask(cb) {
-    cb();
+const { src,dest,watch,parallel } = require('gulp');
+const gulpPug = require('gulp-pug');
+const gulpSass = require('gulp-sass'); 
+const gulpImage = require('gulp-image');
+
+function compressSass() {
+    // コンパイルしたいファイル群
+    return src("assets/scss/style.scss")
+    // pipe()は関数合成
+    .pipe(
+        // コンパイル後のCSSを最適化
+        sass({
+            outputStyle: "compressed"
+        })
+    )
+    // コンパイル後は、ディレクトリ外にCSSを吐き出し＆保存
+    .pipe(dest(""));
 }
-exports.default = defaultTask
+
+function compressImage() {
+    return src("assets/img/*")
+    .pipe(image())
+    .pipe(dest(""));
+}
+
+// 常に監視（タスク自動化）
+function WatchOptimizeFiles() {
+    return watch
+}
+exports.default = WatchOptimizeFiles;
