688c48473cab61a793ce1b05542c018394f68c4c
kurocraft0312 <kurocraft0312@gmail.com>
2020-05-25 00:37:45 +0900

    WIP:カテゴリを含んだニュースタイトル取得中

--- category.php
+++ category.php
@@ -1,14 +1,19 @@
 <?php get_header(); ?>
     <p><?php single_cat_title(); ?>カテゴリ記事の一覧です</p>
 <?php 
-    if(have_posts()):
-        while(have_posts()):the_posts();
+    if(have_posts()) {
+        while(have_posts()) {
+            the_post();
+        }
 ?>
     <!-- ループしたい内容 -->
     <!-- https://noumenon-th.net/programming/2016/06/19/wordpress06/ -->
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     
-
-<?php endwhile; endif; ?>
+<?php 
+    } else {
+        echo "記事が見つかりません。";
+    } 
+?>
 
 <?php get_footer(); ?>
