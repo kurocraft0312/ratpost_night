df4b73d4a45aef364a80f079c9b6cbdadb68cfed
kurocraft0312 <kurocraft0312@gmail.com>
2020-05-31 18:49:01 +0900

    キャッチコピーの設定項目を追加

--- lib/admin/customizer.php
+++ lib/admin/customizer.php
@@ -1,28 +1,31 @@
 <?php
-function theme_customize_register($wp_customize) {
+/***************
+キャッチコピーの追加
+***************/
+function theme_customize_register_catchphrase($wp_customize) {
     // セクション設定（テーマカスタマイザーのメニュー名を設定）
     $wp_customize->add_section('ratpost_theme_options', array(
-        'title' => 'キャッチフレーズ',
+        'title' => 'キャッチコピー',
         'priority' => 10,
-        'description' => 'トップページのキャッチフレーズを設定できます。',
-        'active_callback' => 'is_front_page'// 表示させるページを限定化したいときに使う
+        'description' => 'トップページのキャッチコピーを設定できます。',
     ));
 
     // テーマ設定（メニュー名の中の項目を設定）
     $wp_customize->add_setting('ratpost_theme_settings' , array(
         'default' => '',
-        // 'type' => 'option',
+        'type' => 'option',
         'transport' => 'refresh',
         'sanitize_callback' => 'sanitize_text_field'
     ));
 
     // コントロール設定
     $wp_customize->add_control('ratpost_theme_controls' , array(
-        'label' => 'キャッチフレーズを入力',
-        'description' => 'トップペー',
+        'settings' => 'ratpost_theme_settings',
+        'label' => 'キャッチコピーを入力',
+        'description' => 'トップページに表示するキャッチコピーを入力してください。',
         'section' => 'ratpost_theme_options',
         'type' => 'text'
     ));
 }
-add_action('customize_register','theme_customize_register');
+add_action('customize_register','theme_customize_register_catchphrase');
 ?>
